0c0dd526955564419c77268b5e28d456841b2a3d
24363 <243633732@qq.com>
2015-09-01 16:57:10 +0800

    完成了管理员的增加,删除,修改,等待完成杆塔的管理

--- domain/src/main/java/com/example/repository/VideoRepository.java
+++ domain/src/main/java/com/example/repository/VideoRepository.java
@@ -2,12 +2,6 @@ package com.example.repository;
 
 import rx.Observable;
 
-<<<<<<< HEAD
-=======
-/**
- * Created by 24363 on 2015/8/27.
- */
->>>>>>> origin/master
 public interface VideoRepository {
     Observable<String> getVideoUrl();
 }
